e16819653dcc9c12fa5a93b2791cada477cf01fb
CurL <curlearn@outlook.com>
2021-06-18 20:09:21 +0200

    Bug Fixes - Fixed about page - Fixed navbar wrong adresses

--- globals/js/navbarinjector.js
+++ globals/js/navbarinjector.js
@@ -9,12 +9,12 @@
         <div class="dropdown">
             <button class="active dropbtn" onclick="window.location.href='/projects/main.html'">Projects</button>
             <div class="dropdown-content">
-                <a href="source/ActionEdit/ae.html">ActionEdit</a>
-                <a href="source/EntityEdit/ee.html">EntityEdit</a>
-                <a href="source/GenomeEdit/ge.html">GenomeEdit</a>
-                <a href="source/ItemEdit/ie.html">ItemEdit</a>
-                <a href="source/MagicEdit/me.html">MagicEdit</a>
-                <a href="source/SkillEdit/se.html">SkillEdit</a>
+                <a href="/projects/source/ActionEdit/ae.html">ActionEdit</a>
+                <a href="/projects/source/EntityEdit/ee.html">EntityEdit</a>
+                <a href="/projects/source/GenomeEdit/ge.html">GenomeEdit</a>
+                <a href="/projects/source/ItemEdit/ie.html">ItemEdit</a>
+                <a href="/projects/source/MagicEdit/me.html">MagicEdit</a>
+                <a href="/projects/source/SkillEdit/se.html">SkillEdit</a>
             </div>
         </div>
         <a href="/misc/about.html">About</a>
@@ -22,5 +22,5 @@
 */
 
 // Inject HTML Code
-var inject = "<!--Navbar--> <div class=\"navbar\"> <a href=\"\/index.html\">Home<\/a> <div class=\"dropdown\"> <button class=\"active dropbtn\" onclick=\"window.location.href=\'\/projects\/main.html\'\">Projects<\/button> <div class=\"dropdown-content\"> <a href=\"source\/ActionEdit\/ae.html\">ActionEdit<\/a> <a href=\"source\/EntityEdit\/ee.html\">EntityEdit<\/a> <a href=\"source\/GenomeEdit\/ge.html\">GenomeEdit<\/a> <a href=\"source\/ItemEdit\/ie.html\">ItemEdit<\/a> <a href=\"source\/MagicEdit\/me.html\">MagicEdit<\/a> <a href=\"source\/SkillEdit\/se.html\">SkillEdit<\/a> <\/div> <\/div> <a href=\"\/misc\/about.html\">About<\/a> <\/div>";
+var inject = '<!--Navbar--> <div class=\"navbar\"> <a href=\"\/index.html\">Home<\/a> <div class=\"dropdown\"> <button class=\"active dropbtn\" onclick=\"window.location.href=\'\/projects\/main.html\'\">Projects<\/button> <div class=\"dropdown-content\"> <a href=\"\/projects\/source\/ActionEdit\/ae.html\">ActionEdit<\/a> <a href=\"\/projects\/source\/EntityEdit\/ee.html\">EntityEdit<\/a> <a href=\"\/projects\/source\/GenomeEdit\/ge.html\">GenomeEdit<\/a> <a href=\"\/projects\/source\/ItemEdit\/ie.html\">ItemEdit<\/a> <a href=\"\/projects\/source\/MagicEdit\/me.html\">MagicEdit<\/a> <a href=\"\/projects\/source\/SkillEdit\/se.html\">SkillEdit<\/a> <\/div> <\/div> <a href=\"\/misc\/about.html\">About<\/a> <\/div>';
 document.getElementsByTagName("BODY")[0].insertAdjacentHTML('afterbegin', inject);
