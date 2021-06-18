6a4df535c5ad74bd0cbfa0b776be27557ea10407
CurL <curlearn@outlook.com>
2021-06-18 20:36:11 +0200

    JSHINT Fixes

--- globals/js/jsonhandler.js
+++ globals/js/jsonhandler.js
@@ -1,2 +1,3 @@
+// jshint esversion: 6
 // JSONHANDLER MODULE: Ver. 0.1a
 // Description: Encodes and Parses json files representing a user's RMT World.
--- globals/js/navbarinjector.js
+++ globals/js/navbarinjector.js
@@ -1,3 +1,4 @@
+// jshint esversion: 6
 // Converted using: 
 // 1: https://www.textfixer.com/tools/remove-line-breaks.php
 // 2: http://www.howtocreate.co.uk/tutorials/jsexamples/syntax/prepareInline.html
@@ -22,5 +23,5 @@
 */
 
 // Inject HTML Code
-var inject = '<!--Navbar--> <div class=\"navbar\"> <a href=\"\/index.html\">Home<\/a> <div class=\"dropdown\"> <button class=\"active dropbtn\" onclick=\"window.location.href=\'\/projects\/main.html\'\">Projects<\/button> <div class=\"dropdown-content\"> <a href=\"\/projects\/source\/ActionEdit\/ae.html\">ActionEdit<\/a> <a href=\"\/projects\/source\/EntityEdit\/ee.html\">EntityEdit<\/a> <a href=\"\/projects\/source\/GenomeEdit\/ge.html\">GenomeEdit<\/a> <a href=\"\/projects\/source\/ItemEdit\/ie.html\">ItemEdit<\/a> <a href=\"\/projects\/source\/MagicEdit\/me.html\">MagicEdit<\/a> <a href=\"\/projects\/source\/SkillEdit\/se.html\">SkillEdit<\/a> <\/div> <\/div> <a href=\"\/misc\/about.html\">About<\/a> <\/div>';
+let inject = '<!--Navbar--> <div class=\"navbar\"> <a href=\"\/index.html\">Home<\/a> <div class=\"dropdown\"> <button class=\"active dropbtn\" onclick=\"window.location.href=\'\/projects\/main.html\'\">Projects<\/button> <div class=\"dropdown-content\"> <a href=\"\/projects\/source\/ActionEdit\/ae.html\">ActionEdit<\/a> <a href=\"\/projects\/source\/EntityEdit\/ee.html\">EntityEdit<\/a> <a href=\"\/projects\/source\/GenomeEdit\/ge.html\">GenomeEdit<\/a> <a href=\"\/projects\/source\/ItemEdit\/ie.html\">ItemEdit<\/a> <a href=\"\/projects\/source\/MagicEdit\/me.html\">MagicEdit<\/a> <a href=\"\/projects\/source\/SkillEdit\/se.html\">SkillEdit<\/a> <\/div> <\/div> <a href=\"\/misc\/about.html\">About<\/a> <\/div>';
 document.getElementsByTagName("BODY")[0].insertAdjacentHTML('afterbegin', inject);
