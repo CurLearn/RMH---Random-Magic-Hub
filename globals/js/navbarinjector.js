e522e15e2931abc2329a801a457cb47a73d1123d
CurL <curlearn@outlook.com>
2021-06-19 14:28:57 +0200

    Bug Fix - Fixed issue with displaying active page

--- globals/js/navbarinjector.js
+++ globals/js/navbarinjector.js
@@ -6,9 +6,9 @@
 /* Original:
     <!--Navbar-->
     <div class="navbar">
-        <a href="/index.html">Home</a>
+        <a href="/index.html" id="home">Home</a>
         <div class="dropdown">
-            <button class="active dropbtn" onclick="window.location.href='/projects/main.html'">Projects</button>
+            <button class="dropbtn" id="projects" onclick="window.location.href='/projects/main.html'">Projects</button>
             <div class="dropdown-content">
                 <a href="/projects/source/ActionEdit/ae.html">ActionEdit</a>
                 <a href="/projects/source/EntityEdit/ee.html">EntityEdit</a>
@@ -18,10 +18,18 @@
                 <a href="/projects/source/SkillEdit/se.html">SkillEdit</a>
             </div>
         </div>
-        <a href="/misc/about.html">About</a>
+        <a href="/misc/about.html" id="about">About</a>
     </div>
 */
 
 // Inject HTML Code
-const inject = '<!--Navbar--> <div class=\"navbar\"> <a href=\"\/index.html\">Home<\/a> <div class=\"dropdown\"> <button class=\"active dropbtn\" onclick=\"window.location.href=\'\/projects\/main.html\'\">Projects<\/button> <div class=\"dropdown-content\"> <a href=\"\/projects\/source\/ActionEdit\/ae.html\">ActionEdit<\/a> <a href=\"\/projects\/source\/EntityEdit\/ee.html\">EntityEdit<\/a> <a href=\"\/projects\/source\/GenomeEdit\/ge.html\">GenomeEdit<\/a> <a href=\"\/projects\/source\/ItemEdit\/ie.html\">ItemEdit<\/a> <a href=\"\/projects\/source\/MagicEdit\/me.html\">MagicEdit<\/a> <a href=\"\/projects\/source\/SkillEdit\/se.html\">SkillEdit<\/a> <\/div> <\/div> <a href=\"\/misc\/about.html\">About<\/a> <\/div>';
+const inject = '<!--Navbar--> <div class=\"navbar\"> <a href=\"\/index.html\" id=\"home\">Home<\/a> <div class=\"dropdown\"> <button class=\"dropbtn\" id=\"projects\" onclick=\"window.location.href=\'\/projects\/main.html\'\">Projects<\/button> <div class=\"dropdown-content\"> <a href=\"\/projects\/source\/ActionEdit\/ae.html\">ActionEdit<\/a> <a href=\"\/projects\/source\/EntityEdit\/ee.html\">EntityEdit<\/a> <a href=\"\/projects\/source\/GenomeEdit\/ge.html\">GenomeEdit<\/a> <a href=\"\/projects\/source\/ItemEdit\/ie.html\">ItemEdit<\/a> <a href=\"\/projects\/source\/MagicEdit\/me.html\">MagicEdit<\/a> <a href=\"\/projects\/source\/SkillEdit\/se.html\">SkillEdit<\/a> <\/div> <\/div> <a href=\"\/misc\/about.html\" id=\"about\">About<\/a> <\/div>';
 document.getElementsByTagName("BODY")[0].insertAdjacentHTML('afterbegin', inject);
+
+const path = window.location.pathname.toLowerCase();
+if (path.includes("index"))
+    document.getElementById("home").classList.add("active");
+else if (path.includes("projects"))
+    document.getElementById("projects").classList.add("active");
+else if (path.includes("about"))
+    document.getElementById("about").classList.add("active");
