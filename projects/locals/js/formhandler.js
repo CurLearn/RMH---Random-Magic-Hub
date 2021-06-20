a04ff3c4a8f62e3d923df96ce7c45c0675802b07
CurL <curlearn@outlook.com>
2021-06-20 15:47:03 +0200

    Expanded JSON Support

--- projects/locals/js/formhandler.js
+++ projects/locals/js/formhandler.js
@@ -5,17 +5,21 @@ let prefix = window.prefix;
 
 // Imports
 /* jshint ignore:start */
-let {jsonDisplay, jsonDownload} = await import(prefix + "globals/js/jsonhandler.js");
-let {copyTextToClipboard} = await import(prefix + "globals/js/clipboardhandler.js");
+let {
+    jsonDisplay,
+    jsonDownload
+} = await import(
+    prefix + "globals/js/jsonhandler.js"
+);
+let {
+    copyTextToClipboard
+} = await import(
+    prefix + "globals/js/clipboardhandler.js"
+);
 /* jshint ignore:end */
 
 // Append a value to the json object
 async function appendValue(s, path) {
-    // Check if root is initialized
-    if (typeof json[s.id] == "undefined") {
-        json[s.id] = {};
-    }
-
     // Check if paths are initialized
     for (const key in path) {
         if (Object.hasOwnProperty.call(path, key)) {
@@ -26,23 +30,30 @@ async function appendValue(s, path) {
     }
 
     // Pre-Process the data
-    let append = s.value;
-    if (s.classList.contains("mashed")) {
-        append = append.replaceAll(" ", "");
-    } else if (s.classList.contains("trimmed")) {
-        append = append.trim();
-    } else if (s.classList.contains("dashed")) {
-        const reg = new RegExp("([-])\\1+", "g");
-        append = append.trim().replaceAll(" ", "-").replaceAll(reg, "-");
+    let append = {};
+    if (typeof s.value !== "undefined") {
+        append = s.value;
     }
 
-    if (s.classList.contains("lowercase")) {
-        append = append.toLowerCase();
-    } else if (s.classList.contains("uppercase")) {
-        append = append.toUpperCase();
+    if (typeof s.classList !== "undefined" && $(s).get(0).tagName !== "DIV") {
+        if (s.classList.contains("mashed")) {
+            append = append.replaceAll(" ", "");
+        } else if (s.classList.contains("trimmed")) {
+            append = append.trim();
+        } else if (s.classList.contains("dashed")) {
+            const reg = new RegExp("([-])\\1+", "g");
+            append = append.trim().replaceAll(" ", "-").replaceAll(reg, "-");
+        }
+
+        if (s.classList.contains("lowercase")) {
+            append = append.toLowerCase();
+        } else if (s.classList.contains("uppercase")) {
+            append = append.toUpperCase();
+        }
     }
 
     // Append data to specified location
+
     switch (path.length) {
         case 0:
             json[s.id] = append;
@@ -65,14 +76,16 @@ async function scanJSON(s, path = []) {
 
     if (tag !== "DIV") {
         appendValue(s, path);
-        path.push(s);
+    } else {
+        appendValue(s, path);
+        path.push(s.id);
     }
 
     if ($(this).children().length > 0) {
         return;
     } else {
         $(s).children().each(function() {
-            scanJSON($(this), path);
+            scanJSON(this, path);
         });
     }
 }
@@ -80,6 +93,7 @@ async function scanJSON(s, path = []) {
 // Initializes the script to scan for json-addable objects
 function startScanJSON() {
     json = {};
+    console.clear();
     $("#form").children().each(async function() {
         scanJSON(this);
     });
